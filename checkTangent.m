5debbd905d4181dfafb5dce743b44ed4cade6948
Boyan Naydenov <boyan.naydenov@gmail.com>
2018-01-14 21:17:36 +0100

    Pass xyz to keplerian ECI and obtain DeltaV

---
+++ checkTangent.m
@@ -0,0 +1,14 @@
+function angle = checkTangent(result,num,den)
+    if num > 0 && den <0
+        % segon quadrant
+        angle = pi + result;
+    elseif  num < 0 && den <0
+        % tercer quadrant
+        angle = result + pi;
+    elseif num < 0 && den > 0
+        % quart quadrant
+        angle = result + 2*pi;
+    else
+        angle = result;
+    end
+end
