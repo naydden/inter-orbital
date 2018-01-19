fa66ab3bc09697894d864d4be4957700bf36792a
JosepMSerraMoncunill <josepserra1995@gmail.com>
2018-01-19 10:05:35 +0100

    Function to compute Julian Date, I guess

---
+++ JulianDate.m
@@ -0,0 +1,8 @@
+function JDN=JulianDate(year,month,day)
+
+a = floor((14 - month) / 12);
+y = year + 4800 - a;
+m = month + 12*a - 3;
+
+JDN = day + floor((153*m + 2)/5) + 365*y + floor(y/4) - floor(y/100) + floor(y/400) - 32045;
+end
