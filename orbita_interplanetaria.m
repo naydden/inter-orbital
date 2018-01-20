3004c6f3a0c5afddd00ff709f69568854a525cc1
Boyan Naydenov <boyan.naydenov@gmail.com>
2018-01-20 18:10:27 +0100

    original function interplanetary

--- orbita_interplanetaria.m
+++ orbita_interplanetaria.m
@@ -26,7 +26,7 @@ syms e a theta1;
 eqn1 = (norm(r2)-norm(r1))/(norm(r1)*cos(theta1)-norm(r2)*cos(theta1+deltatheta))-e == 0;
 eqn2 = norm(r1)*(1+e*cos(theta1))/(1-e^2)-a == 0;
 eqn3 = 365.25*a^(3/2)*(2*atan(sqrt((1-e)/(1+e))*tan((theta1+deltatheta)/2))-e*sqrt(1-e^2)*sin(theta1+deltatheta)/(1+e*cos(theta1+deltatheta))-2*atan(sqrt((1-e)/(1+e))*tan(theta1/2))+e*sqrt(1-e^2)*sin(theta1)/(1+e*cos(theta1)))/(2*pi)-deltat == 0;
-S = solve(eqn1,eqn2,eqn3,'Real',true);
+S = solve(eqn1,eqn2,eqn3);
 
 e = S.e; % excentricitat
 a = S.a; % semieix major
