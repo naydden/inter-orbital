ba911678ffeef4dade318cec534bdaa03b809b0e
laurapla <laurazhuh@gmail.com>
2018-01-20 19:52:07 +0100

    Prova ara

--- Test_orbita_interplanetaria.m
+++ Test_orbita_interplanetaria.m
@@ -1,36 +1,42 @@
-% % Cas de la Terra a Mart
+% Cas de la Terra a Mart
 r1 = [0.4537 -0.9094 0];
 r2 = [0.3148 1.5078 0.0239];
 t1 = JulianDate(2020,7,19);
 t2 = JulianDate(2021,1,25);
-% 
-% % Cas de Mart a Jï¿½piter
-% r1 = [1.3277 0.4901 0.0223];
-% r2 = [5.0135 2.1380 0.0505];
+
+% % Cas de Mart a Júpiter
+% r1 = [1.3277 0.4901 -0.0223];
+% r2 = [5.0135 2.1380 -0.0505];
 % t1 = JulianDate(2026,6,5);
 % t2 = JulianDate(2029,4,25);
 % 
-% % Cas 1 de Mart a Jï¿½piter
-% r1 = [1.0707 0.9868 0.0055];
+% % Cas de la Terra a Mart (hiperbòlic)
+% r1 = [-0.9609 0.2466 0];
+% r2 = [0.7285 -1.1980 -0.0430];
+% t1 = JulianDate(2020,3,6);
+% t2 = JulianDate(2020,6,9);
+% 
+% % Cas 1 de Mart a Júpiter
+% r1 = [1.0707 0.9868 -0.0055];
 % r2 = [5.2210 1.4357 0.1109];
 % t1 = JulianDate(2037,10,25);
 % t2 = JulianDate(2039,10,15);
 % 
 % % Cas 2 de la Terra a Mart
-% r1 = [0.9848 0.1338 0];
-% r2 = [0.6797 1.2298 0.0424];
+% r1 = [-0.9848 0.1338 -0];
+% r2 = [0.6797 -1.2298 -0.0424];
 % t1 = JulianDate(2033,3,13);
 % t2 = JulianDate(2033,8,5);
 % 
 % % Cas 3 de la Terra a Mart
-% r1 = [0.5264 0.8316 0.0001];
-% r2 = [0.0108 1.4542 0.0309];
+% r1 = [-0.5264 0.8316 0.0001];
+% r2 = [0.0108 -1.4542 -0.0309];
 % t1 = JulianDate(2031,1,23);
 % t2 = JulianDate(2031,8,1);
 % 
 % % Cas 4 de la Terra a Mart
-% r1 = [0.4342 0.9188 0.0001];
-% r2 = [0.6775 1.3571 0.0118];
+% r1 = [0.4342 -0.9188 0.0001];
+% r2 = [-0.6775 -1.3571 -0.0118];
 % t1 = JulianDate(2025,7,18);
 % t2 = JulianDate(2025,10,21);
 % 
@@ -41,24 +47,24 @@ t2 = JulianDate(2021,1,25);
 % t2 = JulianDate(2023,11,1);
 % 
 % % Cas 6 de Mart a la Terra
-% r1 = [1.5831 0.3913 0.0306];
-% r2 = [0.9123 0.4340 0];
+% r1 = [-1.5831 -0.3913 0.0306];
+% r2 = [0.9123 -0.4340 0];
 % t1 = JulianDate(2033,1,18);
 % t2 = JulianDate(2033,8,28);
 % 
 % % Cas 7 de Mart a la Terra
-% r1 = [1.4166 0.8722 0.0530];
-% r2 = [0.2345 0.9893 0.0001];
+% r1 = [-1.4166 0.8722 0.0530];
+% r2 = [0.2345 -0.9893 -0.0001];
 % t1 = JulianDate(2030,11,20);
 % t2 = JulianDate(2031,7,6);
 % 
-% % Cas 8 de la Terra a Mart (hiperbï¿½lica)
+% % Cas 8 de la Terra a Mart (hiperbòlica)
 % r1 = [0.4383 0.8843 0];
 % r2 = [-0.2082 -1.4582 -0.0255];
 % t1 = JulianDate(2021,11,26);
 % t2 = JulianDate(2022,2,19);
 % 
-% % Cas 9 de la Terra a Mart (hiperbï¿½lica)
+% % Cas 9 de la Terra a Mart (hiperbòlica)
 % r1 = [-0.4079 0.8950 0];
 % r2 = [0.6393 -1.2542 -0.0420];
 % t1 = JulianDate(2022,1,15);
--- orbita_interplanetaria.m
+++ orbita_interplanetaria.m
@@ -1,27 +1,28 @@
 function [a, e, theta1, w, i, Omega] = orbita_interplanetaria(r1,r2,deltat)
 % a: semieix major [AU]
 % e: excentricitat
-% theta1: angle entre el punt Aries i la sonda (o planeta de sortida) [ï¿½]
-% w: argument del periheli [ï¿½]
-% i: inclinaciï¿½ [ï¿½]
-% Omega: longitud eclï¿½ptica del node ascendent [ï¿½]
+% theta1: angle entre el punt Aries i la sonda (o planeta de sortida) [º]
+% w: argument del periheli [º]
+% i: inclinació [º]
+% Omega: longitud eclíptica del node ascendent [º]
 
-% r1: posiciï¿½ heliocï¿½ntrica de la sonda en t1 [AU]
-% r2: posiciï¿½ heliocï¿½ntrica de la sonda en t2 [AU]
+% r1: posició heliocèntrica de la sonda en t1 [AU]
+% r2: posició heliocèntrica de la sonda en t2 [AU]
 % deltat: t2-t1 [dies]
 
-% Cï¿½lcul d'angles
-lambda1 = atan(r1(2)/r1(1)); % [rad]
+% Càlcul d'angles
+lambda1 = atan(r1(2)/r1(1)); %  [rad]
 % lambda1 = checkTangent(lambda1,r1(2),r1(1));
 lambda2 = atan(r2(2)/r2(1)); % [rad]
 % lambda2 = checkTangent(lambda2,r2(2),r2(1));
 beta1 = asin(r1(3)/norm(r1)); % [rad]
 beta2 = asin(r2(3)/norm(r2)); % [rad]
 
-% Cï¿½lcul d'increment d'angles
+% Càlcul d'increment d'angles
 deltalambda = lambda2-lambda1; % [rad]
 deltatheta = acos(sin(beta1)*sin(beta2)+cos(beta1)*cos(beta2)*cos(deltalambda)); % [rad]
-% Resoluciï¿½ de les equacions
+
+% Resolució de les equacions
 syms e a theta1;
 eqn1 = (norm(r2)-norm(r1))/(norm(r1)*cos(theta1)-norm(r2)*cos(theta1+deltatheta))-e == 0;
 eqn2 = norm(r1)*(1+e*cos(theta1))/(1-e^2)-a == 0;
@@ -30,9 +31,9 @@ S = solve(eqn1,eqn2,eqn3);
 
 e = S.e; % excentricitat
 a = S.a; % semieix major
-theta1 = wrapTo360(rad2deg(S.theta1)); % posiciï¿½ inicial en l'ï¿½rbita [ï¿½]
+theta1 = wrapTo360(rad2deg(S.theta1)); % posició inicial en l'òrbita [º]
 
-% Cï¿½lcul de la inclinaciï¿½ per trigonometria esfï¿½rica
+% Càlcul de la inclinació per trigonometria esfèrica
 A = asin(cos(beta2)*sin(deltalambda)/sin(deltatheta)); % [rad]
 i = acos(sin(A)*cos(beta1)); % [rad]
 l = asin(tan(beta1)/tan(i)); % [rad]
@@ -50,8 +51,8 @@ if(i<0)
     w = w+pi;
 end
 
-i = rad2deg(i); % [ï¿½]
-Omega = wrapTo360(rad2deg(Omega)); % [ï¿½]
-w = wrapTo360(rad2deg(w)); % [ï¿½]
+i = rad2deg(i); % [º]
+Omega = wrapTo360(rad2deg(Omega)); % [º]
+w = wrapTo360(rad2deg(w)); % [º]
 
 end
