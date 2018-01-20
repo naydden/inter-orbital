436051976ed09a747519fae6fdebb1c0dd28c487
laurapla <laurazhuh@gmail.com>
2018-01-20 23:02:46 +0100

    A new hope
    Ja funcionen la a, la e i la theta1. Ara falta arreglar els angles.

--- Test_orbita_interplanetaria.m
+++ Test_orbita_interplanetaria.m
@@ -1,12 +1,12 @@
-% Cas de la Terra a Mart
-r1 = [0.4537 -0.9094 0];
-r2 = [0.3148 1.5078 0.0239];
-t1 = JulianDate(2020,7,19);
-t2 = JulianDate(2021,1,25);
-
+% % Cas de la Terra a Mart
+% r1 = [0.4537 -0.9094 0];
+% r2 = [0.3148 1.5078 0.0239];
+% t1 = JulianDate(2020,7,19);
+% t2 = JulianDate(2021,1,25);
+% 
 % % Cas de Mart a Júpiter
 % r1 = [1.3277 0.4901 -0.0223];
-% r2 = [5.0135 2.1380 -0.0505];
+% r2 = [-5.0135 -2.1380 0.0505];
 % t1 = JulianDate(2026,6,5);
 % t2 = JulianDate(2029,4,25);
 % 
@@ -16,12 +16,12 @@ t2 = JulianDate(2021,1,25);
 % t1 = JulianDate(2020,3,6);
 % t2 = JulianDate(2020,6,9);
 % 
-% % Cas 1 de Mart a Júpiter
-% r1 = [1.0707 0.9868 -0.0055];
-% r2 = [5.2210 1.4357 0.1109];
-% t1 = JulianDate(2037,10,25);
-% t2 = JulianDate(2039,10,15);
-% 
+% Cas 1 de Mart a Júpiter
+r1 = [1.0707 0.9868 -0.0055];
+r2 = [-5.2210 1.4357 0.1109];
+t1 = JulianDate(2037,10,25);
+t2 = JulianDate(2039,10,15);
+
 % % Cas 2 de la Terra a Mart
 % r1 = [-0.9848 0.1338 -0];
 % r2 = [0.6797 -1.2298 -0.0424];
@@ -35,7 +35,7 @@ t2 = JulianDate(2021,1,25);
 % t2 = JulianDate(2031,8,1);
 % 
 % % Cas 4 de la Terra a Mart
-% r1 = [0.4342 -0.9188 0.0001];
+% r1 = [0.4342 -0.9188 -0.0001];
 % r2 = [-0.6775 -1.3571 -0.0118];
 % t1 = JulianDate(2025,7,18);
 % t2 = JulianDate(2025,10,21);
@@ -48,7 +48,7 @@ t2 = JulianDate(2021,1,25);
 % 
 % % Cas 6 de Mart a la Terra
 % r1 = [-1.5831 -0.3913 0.0306];
-% r2 = [0.9123 -0.4340 0];
+% r2 = [0.9123 -0.4340 -0];
 % t1 = JulianDate(2033,1,18);
 % t2 = JulianDate(2033,8,28);
 % 
