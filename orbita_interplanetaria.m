675b97e9ed51d7abfdd61ebe84fe21c5adce4906
laurapla <laurazhuh@gmail.com>
2018-01-21 18:28:23 +0100

    (╯°□°）╯︵ ┻━┻
    No entenc per què no funciona :(

--- orbita_interplanetaria.m
+++ orbita_interplanetaria.m
@@ -1,4 +1,4 @@
-function [a, e, theta1, w, i, Omega] = orbita_interplanetaria(r1,r2,deltat)
+function [a, e, theta1, w, i, Omega] = orbita_interplanetaria(r1,r2,deltat,T)
 % a: semieix major [AU]
 % e: excentricitat
 % theta1: angle entre el punt Aries i la sonda (o planeta de sortida) [º]
@@ -9,6 +9,7 @@ function [a, e, theta1, w, i, Omega] = orbita_interplanetaria(r1,r2,deltat)
 % r1: posició heliocèntrica de la sonda en t1 [AU]
 % r2: posició heliocèntrica de la sonda en t2 [AU]
 % deltat: t2-t1 [dies]
+% T: període del planeta d'origen [dies]
 
 % Càlcul d'angles
 lambda1 = atan(r1(2)/r1(1)); %  [rad]
@@ -26,7 +27,7 @@ deltatheta = acos(sin(beta1)*sin(beta2)+cos(beta1)*cos(beta2)*cos(deltalambda));
 syms e a theta1;
 eqn1 = (norm(r2)-norm(r1))/(norm(r1)*cos(theta1)-norm(r2)*cos(theta1+deltatheta))-e == 0;
 eqn2 = norm(r1)*(1+e*cos(theta1))/(1-e^2)-a == 0;
-eqn3 = 365.25*a^(3/2)*(2*atan(sqrt((1-e)/(1+e))*tan((theta1+deltatheta)/2))-e*sqrt(1-e^2)*sin(theta1+deltatheta)/(1+e*cos(theta1+deltatheta))-2*atan(sqrt((1-e)/(1+e))*tan(theta1/2))+e*sqrt(1-e^2)*sin(theta1)/(1+e*cos(theta1)))/(2*pi)-deltat == 0;
+eqn3 = T*a^(3/2)*(2*atan(sqrt((1-e)/(1+e))*tan((theta1+deltatheta)/2))-e*sqrt(1-e^2)*sin(theta1+deltatheta)/(1+e*cos(theta1+deltatheta))-2*atan(sqrt((1-e)/(1+e))*tan(theta1/2))+e*sqrt(1-e^2)*sin(theta1)/(1+e*cos(theta1)))/(2*pi)-deltat == 0;
 S = solve(eqn1,eqn2,eqn3);
 
 e = S.e; % excentricitat
