d50d14ef7589ea3a72cba29d8e3bd32d0c212a4b
laurapla <laurazhuh@gmail.com>
2018-01-23 21:31:03 +0100

    Resultats
    Afegit resultats i semi-redactat de conclusions

--- JulianDate.m
+++ JulianDate.m
@@ -13,6 +13,5 @@ a = floor((14 - month) / 12);
 y = year + 4800 - a;
 m = month + 12*a - 3;
 
-JDN = day + floor((153*m + 2)/5) + 365*y + floor(y/4) 
-      - floor(y/100) + floor(y/400) - 32045;
+JDN = day + floor((153*m + 2)/5) + 365*y + floor(y/4) - floor(y/100) + floor(y/400) - 32045;
 end
--- main.m
+++ main.m
@@ -15,6 +15,14 @@ clc
 mu = 1.3271741784e20;
 dST = 149597870700;
 
+%Venus data
+a_V = 0.723330*dST;
+e_V = 0.006772;
+I_V = 3.3947;
+RAAN_V = 76.6799;
+AP_V = 54.8838;
+M0_V = 50.4161;
+
 %Earth data
 a_E = 149598023000;
 e_E = 0.0167086;
@@ -45,48 +53,48 @@ t1 = JulianDate(2020,7,19);
 t2 = JulianDate(2021,1,25);
 deltat = t2-t1;
 
-% Distances, velocities and true anomalies
-[r1_E,v1_E,theta1_E] = OrbitalVectors (t1,mu,a_E,e_E,I_E,RAAN_E,AP_E,M0_E,t0);
-r1_E = r1_E/dST;
-[r2_M,v2_M,theta2_M] = OrbitalVectors (t2,mu,a_M,e_M,I_M,RAAN_M,AP_M,M0_M,t0);
-r2_M = r2_M/dST;
+% Distances, velocities and true anomalies (O:origin, D:destination)
+[r1_O,v1_O,theta1_O] = OrbitalVectors (t1,mu,a_E,e_E,I_E,RAAN_E,AP_E,M0_E,t0);
+r1_O = r1_O/dST;
+[r2_D,v2_D,theta2_D] = OrbitalVectors (t2,mu,a_M,e_M,I_M,RAAN_M,AP_M,M0_M,t0);
+r2_D = r2_D/dST;
 
 %% Part 1: Heliocentric elliptic trajectory
-[a_S, e_S, theta1, AP_S, I_S, RAAN_S] = orbita_interplanetaria(r1_E,r2_M,deltat);
+[a_S, e_S, theta1, AP_S, I_S, RAAN_S] = orbita_interplanetaria(r1_O,r2_D,deltat);
 E_S=acosd((e_S+cosd(theta1))/(1+e_S*cosd(theta1)));
 M_S=E_S-e_S*sin(E_S);
 a_S = a_S*dST;
 
 %% Part 2: Exit. Geocentric Parking orbit and hyperbolic trajectory and deltaV
 [r1,v1,theta1] = OrbitalVectors (t1,mu,a_S,e_S,I_S,RAAN_S,AP_S,M_S,t1);
-v_inf1=v1-v1_E;
+v_inf1=v1-v1_O;
 [hyperbolaExit, deltaV] = outHyperbola (v_inf1);
 
 %% Part 3: Arrival. Geocentric hyperbolic trajectory and Parking orbit and deltaV
 [r2,v2,theta2] = OrbitalVectors (t2,mu,a_S,e_S,I_S,RAAN_S,AP_S,M_S,t1);
-v_inf2=v2-v2_M;
+v_inf2=v2-v2_D;
 
 %% Part 4: Results Presentation
 N=100;
 A=2;
 t=linspace(t1-A*deltat,t2+A*deltat,(2*A+1)*N);
-r_E=zeros(1,(2*A+1)*N,3);
-r_M=zeros(1,(2*A+1)*N,3);
+r_O=zeros(1,(2*A+1)*N,3);
+r_D=zeros(1,(2*A+1)*N,3);
 r_S=zeros(1,N,3);
 for i=1:(2*A+1)*N
-    [r_E(1,i,:),v1_E,theta1_E] = OrbitalVectors (t(i),mu,a_E,e_E,I_E,RAAN_E,AP_E,M0_E,t0);
-    [r_M(1,i,:),v1_M,theta1_M] = OrbitalVectors (t(i),mu,a_M,e_M,I_M,RAAN_M,AP_M,M0_M,t0);
+    [r_O(1,i,:),v_O,theta_O] = OrbitalVectors (t(i),mu,a_E,e_E,I_E,RAAN_E,AP_E,M0_E,t0);
+    [r_D(1,i,:),v_D,theta_D] = OrbitalVectors (t(i),mu,a_M,e_M,I_M,RAAN_M,AP_M,M0_M,t0);
     if t(i)>=t1 && t(i)<=t2
         [r_S(1,i-A*N,:),v1_S,theta1_S] = OrbitalVectors (t(i),mu,a_S,e_S,I_S,RAAN_S,AP_S,M_S,t1);
     end
 end
-plot3(r_E(1,:,1),r_E(1,:,2),r_E(1,:,3),':b');
+plot3(r_O(1,:,1),r_O(1,:,2),r_O(1,:,3),':b');
 hold on
-plot3(r_E(1,A*N:(A+1)*N,1),r_E(1,A*N:(A+1)*N,2),r_E(1,A*N:(A+1)*N,3),'-b','LineWidth',2);
+plot3(r_O(1,A*N:(A+1)*N,1),r_O(1,A*N:(A+1)*N,2),r_O(1,A*N:(A+1)*N,3),'-b','LineWidth',2);
 hold on
-plot3(r_M(1,:,1),r_M(1,:,2),r_M(1,:,3),':r');
+plot3(r_D(1,:,1),r_D(1,:,2),r_D(1,:,3),':r');
 hold on
-plot3(r_M(1,A*N:(A+1)*N,1),r_M(1,A*N:(A+1)*N,2),r_M(1,A*N:(A+1)*N,3),'-r','LineWidth',2);
+plot3(r_D(1,A*N:(A+1)*N,1),r_D(1,A*N:(A+1)*N,2),r_D(1,A*N:(A+1)*N,3),'-r','LineWidth',2);
 hold on
 plot3(r_S(1,:,1),r_S(1,:,2),r_S(1,:,3),'-g','LineWidth',2);
 axis equal;
