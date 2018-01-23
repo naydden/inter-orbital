6ada966e1a3e1ec0a9e17dc49ac3b3dbfdadd586
laurapla <laurazhuh@gmail.com>
2018-01-23 03:58:38 +0100

    Orbital plots

--- Test_orbita_interplanetaria.m
+++ Test_orbita_interplanetaria.m
@@ -10,11 +10,11 @@
 % t1 = JulianDate(2026,6,5);
 % t2 = JulianDate(2029,4,25);
 % 
-% % Cas de la Terra a Mart (hiperbòlic)
-% r1 = [-0.9609 0.2466 0];
-% r2 = [0.7285 -1.1980 -0.0430];
-% t1 = JulianDate(2020,3,6);
-% t2 = JulianDate(2020,6,9);
+% Cas de la Terra a Mart (hiperbòlic)
+r1 = [-0.9609 0.2466 0];
+r2 = [0.7285 -1.1980 -0.0430];
+t1 = JulianDate(2020,3,6);
+t2 = JulianDate(2020,6,9);
 % 
 % % Cas 1 de Mart a Júpiter
 % r1 = [1.0707 0.9868 -0.0055];
@@ -28,11 +28,11 @@
 % t1 = JulianDate(2033,3,13);
 % t2 = JulianDate(2033,8,5);
 % 
-% Cas 3 de la Terra a Mart
-r1 = [-0.5264 0.8316 0.0001];
-r2 = [0.0108 -1.4542 -0.0309];
-t1 = JulianDate(2031,1,23);
-t2 = JulianDate(2031,8,1);
+% % Cas 3 de la Terra a Mart
+% r1 = [-0.5264 0.8316 0.0001];
+% r2 = [0.0108 -1.4542 -0.0309];
+% t1 = JulianDate(2031,1,23);
+% t2 = JulianDate(2031,8,1);
 % 
 % % Cas 4 de la Terra a Mart
 % r1 = [0.4342 -0.9188 -0.0001];
@@ -73,3 +73,39 @@ t2 = JulianDate(2031,8,1);
 deltat = t2-t1;
 
 [a, e, theta1, w, i, Omega] = orbita_interplanetaria(r1,r2,deltat)
+
+lambda1 = atan(r1(2)/r1(1)); %  [rad]
+lambda1 = checkTangent(lambda1,r1(2),r1(1));
+lambda2 = atan(r2(2)/r2(1)); % [rad]
+lambda2 = checkTangent(lambda2,r2(2),r2(1));
+beta1 = asin(r1(3)/norm(r1)); % [rad]
+beta2 = asin(r2(3)/norm(r2)); % [rad]
+deltalambda = wrapTo2Pi(lambda2-lambda1); % [rad]
+deltatheta = acos(sin(beta1)*sin(beta2)+cos(beta1)*cos(beta2)*cos(deltalambda)); % [rad]
+
+% Mart
+a_M = 227939200000/149597870700;
+e_M = 0.0934;
+t_M=0:pi/40:2*pi;
+R_M=a_M*(1-e_M^2)./(1+e_M*cos(t_M));
+polar(t_M,R_M);
+hold on
+
+% Terra
+a_E = 149598023000/149597870700;
+e_E = 0.0167086;
+t_E=0:pi/40:2*pi;
+R_E=a_E*(1-e_E^2)./(1+e_E*cos(t_E));
+polar(t_E,R_E);
+hold on
+
+% Interplanetària
+t=deg2rad(theta1):pi/40:deg2rad(theta1)+deltatheta;
+if e<1
+    R=a*(1-e^2)./(1+e*cos(t));
+else
+    R=a*(e^2-1)./(1+e*cos(t));
+end
+polar(t,R);
+
+legend('Mars','Earth','Interplanetary')
